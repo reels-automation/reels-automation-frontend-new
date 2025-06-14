c8d895d588b1884c3bb54f1fc9d4f57d8e1486db
CuackXI <santinoandreatta@gmail.com>
2025-06-14 19:31:15 +0000

    Fixed commits to main and transferred them to branch

--- src/pages/guest/guest.tsx
+++ src/pages/guest/guest.tsx
@@ -1,24 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Guest = () => {
-
-
-
   return (
     <div className="min-h-screen flex flex-col">
-      <main className="flex-grow px-4 py-8 mt-10">
-        <div className="flex">
-
-            <div className="rounded-md flex w-1/2 justify-center items-center p-4 bg-stone-200 m-4">
-                <img className="" src="https://madridtrauma.com/wp-content/uploads/2016/12/perfil-00-HomeroValencia.png" alt="" />
-            </div>
-
-            <div className="w-1/2 flex p-4 m-4 bg-stone-200 rounded-md ml-4">
-              <div className="flex flex-col justify-start">
-                <h1 className="font-bold text-5xl">Aprende ahora</h1>
-                <p className="font-bold mt-5 text-xl">Unete hoy.</p>
+      <main className="flex-grow mt-10">
+        <div className="flex sm:flex-row flex-col">
+          <div className="rounded-md flex sm:justify-center sm:items-center p-4 m-4 sm:w-1/2">
+            <img
+              className="w-16 h-auto sm:w-96"
+              src="https://lens-storage.storage.googleapis.com/png/bb4352e639a04511875104672e95af13"
+              alt=""
+            />
+          </div>
+          <div className="flex p-4 m-4 rounded-md ml-4 sm:w-1/2">
+            <div className="flex flex-col justify-start">
+              <h1 className="font-bold lg:text-6xl md:text-4xl sm:text-2xl text-5xl">Aprende ahora</h1>
+              <p className="font-bold mt-5 lg:text-3xl md:text-xl sm:text-lg text-xl">Unite hoy.</p>
+              <Button asChild variant={"default"} className="mt-5 rounded-full">
+                <Link to="/register">Crear una cuenta</Link>
+              </Button>
+              <div className="flex items-center my-5">
+                <div className="flex-1 h-px bg-gray-400" />
+                <p className="mx-4 text-gray-500 text-sm">o</p>
+                <div className="flex-1 h-px bg-gray-400" />
               </div>
+              <p className="font-bold text-lg">¿Ya tenés una cuenta?</p>
+              <Button asChild variant={"outline"} className="mt-1 rounded-full">
+                <Link to="/login">Iniciar Sesión</Link>
+              </Button>
             </div>
-
+          </div>
         </div>
       </main>
     </div>
