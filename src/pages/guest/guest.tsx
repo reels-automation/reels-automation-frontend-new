666c66dec10fd45182b3d5913e5bf0837d94d6e3
CuackXI <santinoandreatta@gmail.com>
2025-06-26 19:25:26 +0000

    Fixee centrar verticalmente la pagina de guest

--- src/pages/guest/guest.tsx
+++ src/pages/guest/guest.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const Guest = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <main className="flex-grow mt-10">
+    <div className="min-h-screen flex items-center justify-center">
+      <main className="w-full max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex sm:flex-row flex-col">
           <div className="rounded-md flex sm:justify-center sm:items-center p-4 m-4 sm:w-1/2">
             <img
@@ -15,7 +15,7 @@ const Guest = () => {
           </div>
           <div className="flex p-4 m-4 rounded-md ml-4 sm:w-1/2">
             <div className="flex flex-col justify-start">
-              <h1 className="font-bold lg:text-6xl md:text-4xl sm:text-2xl text-5xl">Aprende ahora</h1>
+              <h1 className="font-bold lg:text-6xl md:text-4xl sm:text-2xl text-5xl">Crea videos ahora</h1>
               <p className="font-bold mt-5 lg:text-3xl md:text-xl sm:text-lg text-xl">Unite hoy.</p>
               <Button asChild variant={"default"} className="mt-5 rounded-full">
                 <Link to="/register">Crear una cuenta</Link>
